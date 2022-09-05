05b543a9c73c8c2aeaa90f00545e57bdc51caf29
Vinicius Souza <me@viniciusals.com>
2022-09-04 20:53:43 -0500

    Creates service to handle technologies requests

---
+++ src/services/TechnologiesService.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+import IResponse from '../types/IResponse';
+import ITechnologies from '../types/ITechnologies';
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:1337/api/',
+    headers: {
+        'Content-type': 'application/json'
+    }
+});
+
+const findAll = async () => {
+    const response = await apiClient.get<IResponse<ITechnologies>>('/technologies');
+    return response.data.data;
+};
+
+const findByID = async (id: any) => {
+    const response = await apiClient.get<IResponse<ITechnologies>>(`/technologies/${id}`);
+    return response.data.data;
+};
+
+const TechnologiesService = {
+    findAll,
+    findByID
+};
+
+export default TechnologiesService;
