2b2d56e5b8a099ae8c96f9f90b8a99b04e2019f1
Vinicius Souza <me@viniciusals.com>
2022-09-11 13:16:06 -0500

    Creates object for projects section

---
+++ src/sections/Projects/Projects.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Content from '../../components/Content/Content';
+
+export default function Projects() {
+    return (
+        <Content>
+            <h2>Projects</h2>
+        </Content>
+    );
+}
