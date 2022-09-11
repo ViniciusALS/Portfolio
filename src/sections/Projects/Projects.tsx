404783580cbb876bcd3355ccc1d811eae4384182
Vinicius Souza <me@viniciusals.com>
2022-09-11 13:26:49 -0500

    Fetches project data to populate projects section

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Content from '../../components/Content/Content';
+import ProjectsService from '../../services/TechnologiesService';
+import IProjects from '../../types/ITechnologies';
 
 export default function Projects() {
+    const [projects, setProjects] = useState<IProjects[]>([]);
+
+    useEffect(() => {
+        ProjectsService.findAll()
+            .then(response => setProjects(response))
+            .catch(); // TODO: handle response error
+    });
+
     return (
         <Content>
             <h2>Projects</h2>
+
+            <div>
+                <h3>Project Title</h3>
+                <div>Image</div>
+                <div>Project description</div>
+                <div>Technologies list</div>
+                <div>learn more</div>
+                <div>Github link</div>
+                <div>Live project</div>
+            </div>
         </Content>
     );
 }
