5b7704389b1adbbbfa524bea2a45e01d6861dc73
Vinicius Souza <me@viniciusals.com>
2022-09-11 13:12:54 -0500

    Creates carousel object

---
+++ src/components/TechCarousel/TechCarousel.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import Slider from 'react-slick';
+import './slick-carousel.css';
+
+import TechCard from '../../components/TechCard/TechCard';
+
+interface Props {
+    technologies: any[]
+}
+
+export default function TechCarousel(props: Props) {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        swipeToSlide: true
+    };
+
+    const { technologies } = props;
+
+    return (
+        <div>
+            <Slider {...settings}>
+                {
+                    technologies.map(
+                        tech => <TechCard>{tech.attributes.Name}</TechCard>
+                    )
+                }
+            </Slider>
+        </div>
+    );
+}
