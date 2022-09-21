1af2fd9da4670086fac32020bace22eb0ff858dd
Vinicius Souza <me@viniciusals.com>
2022-09-21 02:57:21 -0500

    Refactors projectCard to display project cover image

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -9,10 +9,14 @@ interface Props {
 export default function ProjectCard(props: Props) {
     const { project } = props;
 
+    const coverImgURL = project.attributes.Cover.data.attributes.url;
+    const coverImgAltText = project.attributes.Cover.data.attributes.name;
+    const imgURL = process.env.REACT_APP_IMG_URL_PREFIX! + coverImgURL;
+
     return (
         <div className={styles.card}>
             <h3>{project.attributes.Title}</h3>
-            <div>{project.attributes.Cover.data.attributes.url}</div>
+            <img src={imgURL} alt={coverImgAltText}/>
             <div>{project.attributes.Description}</div>
             <div>Technologies list</div>
             <div>learn more</div>
