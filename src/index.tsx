1ef30f97cbff9655d80771e5fe7f59622d0948ba
Vinicius Souza <me@viniciusals.com>
2022-09-21 02:55:51 -0500

    Adds routes for project post and 404 pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './reset.css';
 import './index.css';
 import Home from './pages/Home/Home';
+import ProjectPost from './pages/ProjectPost/ProjectPost';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/404" element={<Home/>}/>
+            <Route path="/project/:id" element={<ProjectPost/>}/>
+            <Route path="*" element={<PageNotFound/>}/>
         </Routes>
     </BrowserRouter>,
     document.getElementById('root')
