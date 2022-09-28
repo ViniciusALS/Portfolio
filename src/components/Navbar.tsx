90fb720e7a3639d70c195e2050f9ca966b3b8ca9
Vinicius Souza <me@viniciusals.com>
2022-09-27 23:56:31 -0500

    Updata navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { shipIcon } from '../assets';
 
+// TODO: Make link go to top of home page
 export default function Navbar() {
     return(
-        <h1>Navbar</h1>
+        <nav className='w-full bg-darkerBlue py-[10px] flex items-cente content'>
+            <a href='#'>
+                <div className='w-[35px] h-[35px] xs:w-[40px] xs:h-[40px] bg-blueTwo'/>
+            </a>
+        </nav>
     );
 }
