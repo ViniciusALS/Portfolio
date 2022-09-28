f61e0594963bf9f5026bf4755dcffb5e1862faf4
Vinicius Souza <me@viniciusals.com>
2022-09-27 23:56:01 -0500

    Removes error page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,10 +1,8 @@
-import Error from './Error';
 import Home from './Home';
 import NotFound from './NotFound';
 import Project from './Project';
 
 export {
-    Error,
     Home,
     NotFound,
     Project
