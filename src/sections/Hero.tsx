efbf7dd73f06f80d1cf8a4f543e3b9501c25eb9f
Vinicius Souza <me@viniciusals.com>
2022-09-27 23:55:04 -0500

    Update Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import {linkedIn, twitter, resume} from '../assets';
+import { Ship, Wave, ContactButton } from '../components';
 
 export default function Hero() {
     return(
-        <h1>Hero</h1>
+        <section className='w-[100vw] h-[100vh] bg-darkBlue'>
+            <h1 className='flex flex-col font-roboto font-bold text-5xl text-nearWhite content pt-[20vh]'>
+                <span>Hi,</span>
+                <span>I am Vinicius</span>
+            </h1>
+
+            {/* <div>
+                <Wave/>
+                <Ship/>
+                <Wave/>
+                <Wave/>
+            </div> */}
+
+            <div className='flex flex-row w-full justify-evenly content'>
+                <ContactButton
+                    src={linkedIn}
+                    alt='LinkedIn'
+                    text='Add me on LinkedIn'/>
+
+                <ContactButton
+                    src={twitter}
+                    alt='Twitter'
+                    text='Follow me on Twitter'/>
+
+                <ContactButton
+                    src={resume}
+                    alt='Resume'
+                    text='Download my resume'/>
+            </div>
+
+        </section>
     );
 }
