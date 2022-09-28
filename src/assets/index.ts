0dce5463204611c7b022318674d1cb7ad131839a
Vinicius Souza <me@viniciusals.com>
2022-09-27 23:54:33 -0500

    Adds wave.svg and resume.pdf files

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,11 @@
-import shipIcon from './shipIcon.svg';
+import shipIcon from './ship-icon.svg';
 import ship from './ship.svg';
 import email from './email.svg';
 import linkedIn from './linkedIn.svg';
 import resume from './resume.svg';
 import twitter from './twitter.svg';
-
+import wave from './wave.svg';
+import currentResume from './resume-samples.pdf';
 
 export {
     shipIcon,
@@ -12,5 +13,7 @@ export {
     email,
     linkedIn,
     resume,
-    twitter
+    twitter,
+    wave,
+    currentResume
 };
