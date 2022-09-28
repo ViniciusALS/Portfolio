c3ad5bb7cfdad6b7eb1795019d31d650d42c32a3
Vinicius Souza <me@viniciusals.com>
2022-09-27 23:55:12 -0500

    Update footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function Footer() {
     return(
-        <h1>Footer</h1>
+        <section className='content bg-darkBlue font-poppins text-nearWhite'>
+            <h3>/viniciusalsouza</h3>
+            <h3>@vinnie_als</h3>
+            <h3>contact@viniciusals.com</h3>
+        </section>
     );
 }
