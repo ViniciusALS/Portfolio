f6ef996bb7aef6ffbae59db2434c983b1004bcc9
Vinicius Souza <me@viniciusals.com>
2022-09-29 01:20:00 -0500

    Fixes navbar on top of page and links home button to top of page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shipIcon } from '../assets';
 
-// TODO: Make link go to top of home page
+// TODO: Make link go to home page if on another page
 export default function Navbar() {
     return(
-        <nav className='w-full bg-darkerBlue py-[10px] flex items-cente content'>
+        <nav className='w-full bg-darkerBlue py-[10px] flex items-cente content fixed'>
             <a href='#'>
                 <div className='w-[35px] h-[35px] xs:w-[40px] xs:h-[40px] bg-blueTwo'/>
             </a>
