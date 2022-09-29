558f3a4d31f5d186490ba34633d46f87adfa0fb5
Vinicius Souza <me@viniciusals.com>
2022-09-29 01:52:08 -0500

    Brings navbar to top of z-index

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { shipIcon } from '../assets';
 // TODO: Make link go to home page if on another page
 export default function Navbar() {
     return(
-        <nav className='w-full bg-darkerBlue py-[10px] flex items-cente content fixed'>
+        <nav className='w-full bg-darkerBlue py-[10px] flex items-cente content fixed z-10'>
             <a href='#'>
                 <div className='w-[35px] h-[35px] xs:w-[40px] xs:h-[40px] bg-blueTwo'/>
             </a>
