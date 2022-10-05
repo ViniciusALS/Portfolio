587d5b01cb2b6a1727f69bb4c8cc8472f3522ca7
Vinicius Souza <me@viniciusals.com>
2022-10-05 02:50:21 -0500

    Displays technology name and image on a card

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import ITechnology from '../types/ITechnology';
 
-export default function TechCard() {
-    return(
-        <h1>TechCard</h1>
+interface Props {
+    technology: ITechnology
+}
+
+export default function TechCard(props: Props) {
+
+    const techImgURL = props.technology.attributes.Logo?.data.attributes.url;
+    const imgURL = 'http://localhost:1337' + techImgURL;
+
+    return (
+        <div className='flex flex-row items-center px-4'>
+            <img src={imgURL} alt='' className='h-8 px-2'/>
+            <h3>{props.technology.attributes.Name}</h3>
+        </div>
     );
 }
