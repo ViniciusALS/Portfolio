56660e84b6d9becc7b512b88e16dfd9f52530250
Vinicius Souza <me@viniciusals.com>
2022-10-04 20:25:14 -0500

    Retrieves technologies data from db and renders technologies carousel

--- src/sections/Technologies.tsx
+++ src/sections/Technologies.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { TechCarousel } from '../components';
+import TechnologiesService from '../Services/TechnologiesService';
+import ITechnology from '../types/ITechnology';
 
 export default function Technologies() {
-    return(
-        <h1>Technologies</h1>
+    const [technologies, setTechnologies] = useState<ITechnology[]>([]);
+
+    useEffect(() => {
+        TechnologiesService.findAll()
+            .then(response => setTechnologies(response));
+    }, []);
+
+    return (
+        <section>
+            <TechCarousel technologies={technologies}/>
+        </section>
     );
 };
