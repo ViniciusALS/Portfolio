54a7e719621fea30533257ff3b359ecb7cae23c6
Vinicius Souza <me@viniciusals.com>
2022-10-04 19:54:00 -0500

    Creates component for ship image

--- src/components/Ship.tsx
+++ src/components/Ship.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import { ship } from '../assets';
 
 export default function Ship() {
     return(
-        <h1>Ship</h1>
+        <img
+            src={ship}
+            alt=''
+            className='absolute right-0 left-0 ss:left-auto bottom-[23vh] h-[18rem] ss:h-[20rem] md:h-[22rem] lg:h-[25rem] px-0 ss:px-[10%] md:px-[15%] lg:px-[20%] mx-auto ss:mx-0'/>
     );
 }
