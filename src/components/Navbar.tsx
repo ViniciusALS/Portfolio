9ffcb577bba47648e71671b9cfdb94957aa8c02c
Vinicius Souza <me@viniciusals.com>
2022-11-30 03:24:31 -0500

    Update navbar to link to home screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { shipIcon } from '../assets';
+import {Link} from 'react-router-dom';
 
-// TODO: Make link go to home page if on another page
 export default function Navbar() {
     return(
         <nav className='w-full bg-darkerBlue py-[10px] flex items-cente content fixed z-10'>
-            <a href='#'>
+            <Link to='/'>
                 <div className='w-[35px] h-[35px] xs:w-[40px] xs:h-[40px] bg-blueTwo'/>
-            </a>
+            </Link>
         </nav>
     );
 }
