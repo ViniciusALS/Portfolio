3e50dc8d6981177ac7597b409c46fb2a4255271a
Vinicius Souza <me@viniciusals.com>
2022-11-30 03:23:29 -0500

    Updates Footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import {linkedIn, twitter, email} from '../assets';
 
 export default function Footer() {
     return(
-        <section className='content bg-darkBlue font-poppins text-nearWhite'>
-            <h3>/viniciusalsouza</h3>
-            <h3>@vinnie_als</h3>
-            <h3>contact@viniciusals.com</h3>
+        <section className='content bg-darkBlue mt-12 py-12 flex flex-col gap-3'>
+            <a
+                href='https://www.linkedin.com/in/viniciusalsouza/'
+                target='_blank'
+                className='flex flex-row items-center'>
+
+                <img className='h-[1.4em] pr-2' src={linkedIn} alt="Vinicius' linkedin"/>
+                <h3 className='text-nearWhite font-poppins'>/viniciusalsouza</h3>
+            </a>
+
+            <a
+                href='https://twitter.com/Vinnie_als'
+                target='_blank'
+                className='flex flex-row items-center'>
+
+                <img className='h-[1.4em] pr-2' src={twitter} alt="Vinicius' twitter"/>
+                <h3 className='text-nearWhite font-poppins'>@vinnie_als</h3>
+            </a>
+
+            <span
+                className='flex flex-row items-center'>
+
+                <img className='h-[1.4em] pr-2' src={email} alt="Vinicius' email"/>
+                <h3 className='text-nearWhite font-poppins'>contact@viniciusals.com</h3>
+            </span>
         </section>
     );
 }
