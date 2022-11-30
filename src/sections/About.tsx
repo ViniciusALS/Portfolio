146a628961ed11843080e023e0cff62539ffddfc
Vinicius Souza <me@viniciusals.com>
2022-11-30 03:23:48 -0500

    Adds vertical margin to about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function About() {
     return(
-        <section className='content flex flex-row justify-center w-full text-center font-roboto font-bold leading-relaxed text-xl sm:leading-loose sm:text-2xl lg:text-3xl lg:leading-loose'>
+        <section className='content flex flex-row justify-center w-full text-center font-roboto font-bold leading-relaxed text-xl sm:leading-loose sm:text-2xl lg:text-3xl lg:leading-loose my-12'>
             <h3>
                 I am focused in learning <br className='md:hidden'/><span className='text-highlightPink'>new Technologies</span> <br/>
                 that give me the power to <br className='md:hidden'/><span className='text-highlightGreen'>create new things</span> <br/>
