1ee34c72b0c851fde0cc2fe0af47954b04a401cb
Vinicius Souza <me@viniciusals.com>
2022-11-30 03:21:27 -0500

    Adds arrow svg image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,13 +1,15 @@
-import shipIcon from './ship-icon.svg';
-import ship from './ship.svg';
+import arrow from './arrow.svg';
 import email from './email.svg';
 import linkedIn from './linkedIn.svg';
+import currentResume from './resume-samples.pdf';
 import resume from './resume.svg';
+import shipIcon from './ship-icon.svg';
+import ship from './ship.svg';
 import twitter from './twitter.svg';
 import wave from './wave.svg';
-import currentResume from './resume-samples.pdf';
 
 export {
+    arrow,
     shipIcon,
     ship,
     email,
