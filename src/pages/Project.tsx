c6b267039ec99ab301c6810a3c351145eb2cb679
Vinicius Souza <me@viniciusals.com>
2022-12-16 01:17:20 -0500

    Update project page

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Navbar } from '../components';
+import { Footer } from '../sections';
+import { useLocation } from 'react-router-dom';
+import ProjectsService from '../Services/ProjectsService';
+import IProject from '../types/IProject';
 
 export default function Project() {
+
+    const [project, setProject] = useState<any>();
+
+    const path = useLocation().pathname;
+    const slug = path.split('/')[2];
+
+    useEffect(() => {
+        ProjectsService.findBySlug(slug)
+            .then(response => setProject(response));
+    }, []);
+
+    // const title = project?.attributes.Title;
+
+    console.log(project[0].attributes);
+
     return(
-        <h1>Project</h1>
+        <>
+            <Navbar/>
+            <div className='h-[80vh]'>
+                {/* <h1 className='pt-[100px]'>{title}</h1> */}
+            </div>
+            <Footer/>
+        </>
     );
 }
