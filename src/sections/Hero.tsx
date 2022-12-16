c5ef42c45d11df8ca8f4317f94afb58082835500
Vinicius Souza <me@viniciusals.com>
2022-12-16 01:17:51 -0500

    Updates css classes on hero section to make text white

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,9 +6,9 @@ import '../styles/hero.css';
 export default function Hero() {
     return(
         <section className='w-[100vw] h-[100vh] bg-darkBlue relative overflow-hidden flex'>
-            <h1 className='flex flex-col font-roboto font-bold text-4xl ss:text-5xl md:text-6xl text-nearWhite absolute top-[22vh] ss:top-[30vh] px-[10%] md:px-[15%] lg:px-[20%]'>
-                <span>Hi,</span>
-                <span>I am Vinicius</span>
+            <h1 className='flex flex-col font-roboto font-bold text-4xl ss:text-5xl md:text-6xl absolute top-[22vh] ss:top-[30vh] px-[10%] md:px-[15%] lg:px-[20%]'>
+                <span className='text-nearWhite'>Hi,</span>
+                <span className='text-nearWhite'>I am Vinicius</span>
             </h1>
 
             <Wave className='wave3' waveSpeed={50}/>
