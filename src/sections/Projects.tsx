1d23d04770f9ff4be97e8edac3ef8847216c9a73
Vinicius Souza <me@viniciusals.com>
2022-12-16 01:18:28 -0500

    Populate projects section with api data

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ProjectCard } from '../components';
+import ProjectsService from '../Services/ProjectsService';
+import IProject from '../types/IProject';
 
 export default function Projects() {
+    const [projects, setProjects] = useState<IProject[]>([]);
+
+    useEffect(() => {
+        ProjectsService.findAll()
+            .then(response => setProjects(response));
+    }, []);
+
     return(
-        <h1>Projects</h1>
+        <section>
+            <h1 className='pt-12 font-roboto uppercase text-2xl flex items-center justify-center pb-6 after:h-[2px] after:bg-black after:flex-1 before:flex-1 before:h-[2px] before:bg-black after:ml-5 before:mr-5 content'>Projects</h1>
+
+            {
+                projects.map(project =>
+                    <ProjectCard key={project.id} project={project}/>
+                )
+            }
+        </section>
+
     );
 };
